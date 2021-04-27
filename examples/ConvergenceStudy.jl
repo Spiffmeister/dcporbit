85fbccd31a1616cd411edb80fc913e529473412b
Dean Muir <u6257479@anu.edu.au>
2021-04-27 12:40:29 +1000

    Updated the "example"

--- examples/ConvergenceStudy.jl
+++ examples/ConvergenceStudy.jl
@@ -1,37 +1,44 @@
-
-
-
-
-
 #=
-    MAGNETIC FIELDS
+    Performing convergence study
 =#
-function Bfield(x)
-    α = pi/6
-    if x[3] > 0
-        B = [cos(α),-sin(α),0]
-    else
-        B = [cos(α),sin(α),0]
-    end
-    return B
-end
 
+# Is this a guiding center intialisation?
+guidingcenter = false
 
-p = particle(x₀,v₀,t₀,Δt,MagneticForce,Bfield,Bfield(x₀),1)
+gc₀ = [0.,0.,0.]
+v₀ = [1.,0.,0.]
 
+t₀ = 0.
+t_f = 1.
+Δt = 1.e-1
+tol = 1.e-14
 
-#=
-    EVENT FUNCTION
-=#
+# function Bfield(x)
+#     α = pi/6
+#     if x[3] > 0
+#         B = [cos(α),-sin(α),0.]
+#     else
+#         B = [cos(α),sin(α),0.]
+#     end
+#     return B
+# end
+
+α = pi/6
+B₁(x) = [cos(α),-sin(α),0.]
+B₂(x) = [cos(α),sin(α),0.]
+
+Bfield = [B₁,B₂]
+
 function event(xv,t)
     chk = xv[3,2]
     return chk
 end
 
 
+p = particle(x₀,v₀,t₀,Δt,MagneticForce,Bfield,Bfield(x₀),1)
 
 
-
+integrate(p,t_f,eventfn=eventfn)
 
 
 
